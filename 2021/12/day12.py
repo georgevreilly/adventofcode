0134ecf258c8cfc0d4a6e380e335f57a2d45d15c
George V. Reilly <george@reilly.org>
2021-12-11 22:33:02 -0800

    Day 12 solution

--- 2021/12/day12.py
+++ 2021/12/day12.py
@@ -2,6 +2,8 @@
 
 from __future__ import annotations
 
+from collections import defaultdict, deque
+
 DAY = 12
 # https://adventofcode.com/2021/day/12
 
@@ -21,7 +23,7 @@ def parse_args() -> argparse.Namespace:
         input_filename=None,
         real=True,
         verbose=False,
-        custom=False,
+        custom=0,
     )
     group = parser.add_mutually_exclusive_group()
     group.add_argument(
@@ -35,8 +37,8 @@ def parse_args() -> argparse.Namespace:
         "--steps", "-s", type=int,
         help="Number of steps")
     parser.add_argument(
-        "--custom", "-C", action="store_true",
-        help="Use custom data")
+        "--custom", "-C", type=int,
+            help="Use custom data")
     parser.add_argument(
         "--verbose", "-v", action="store_true",
         help="More verbose logging")
@@ -54,42 +56,117 @@ def read_data(input_filename: str):
         return f.readlines()
 
 
-def parse_data(text_data: list[str]) -> list[list[int]]:
-    return [[int(d) for d in line.strip()]
-            for line in text_data]
+def parse_data(text_data: list[str]) -> dict[str, list[str]]:
+    pairs = defaultdict(list)
+    for line in text_data:
+        src, dst = line.strip().split("-")
+        pairs[src].append(dst)
+        pairs[dst].append(src)
+    return pairs
 
 
 def load_data(namespace) -> list[list[int]]:
-    if namespace.custom:
-        text_data = ["-1"]
-        data = parse_data(text_data)
+    if namespace.custom == 1:
+        text_data = [
+            "dc-end",
+            "HN-start",
+            "start-kj",
+            "dc-start",
+            "dc-HN",
+            "LN-dc",
+            "HN-end",
+            "kj-sa",
+            "kj-HN",
+            "kj-dc",
+        ]
+    elif namespace.custom == 2:
+        text_data = [
+            "fs-end",
+            "he-DX",
+            "fs-he",
+            "start-DX",
+            "pj-DX",
+            "end-zg",
+            "zg-sl",
+            "zg-pj",
+            "pj-he",
+            "RW-he",
+            "fs-DX",
+            "pj-RW",
+            "zg-RW",
+            "start-pj",
+            "he-WI",
+            "zg-he",
+            "pj-fs",
+            "start-RW",
+        ]
     else:
         text_data = read_data(namespace.input_filename)
         logging.info("%s", namespace.input_filename)
-        logging.debug("%s", text_data)
-        data = parse_data(text_data)
+
+    logging.debug("%s", text_data)
+    data = parse_data(text_data)
     return data
 
 
-def grid_to_str(grid: list[list[int]], prefix="") -> str:
-    return prefix + ("\n" + prefix).join(
-        "".join([str(n) for n in row]) for row in grid) + "\n"
+def explore1(node: str, pairs: dict[str, list[str]], seen: set[str], path: list[str]):
+    logging.debug(f"explore1: {path=} {node=} {seen=} dsts={pairs[node]}")
+    if node == "end":
+        logging.debug(f"\tPath: {path}")
+        yield path
+    for dst in pairs[node]:
+        if dst in seen and dst.islower():
+            continue
+        yield from explore1(dst, pairs, seen | {node}, path + [dst])
 
 
-def compute1(grid: list[list[int]]) -> int:
-    total = 0
-    return total
+def compute1(pairs: dict[str, list[str]]) -> int:
+    paths = [path for path in explore1("start", pairs, set(), ["start"])]
+    for path in paths:
+        logging.debug(f'{",".join(path)}')
+    return len(paths)
 
 
-def compute2(grid: list[list[int]]) -> int:
-    total = 0
-    return total
+def explore2(node: str, pairs: dict[str, list[str]], seen: dict[str, int], path: list[str]):
+    if node == "end":
+        logging.debug(f"\tPath: {path}")
+        yield path
+        return
+
+    seen = seen.copy()
+    seen[node] += 1
+    logging.debug(f"explore2: path={','.join(path)} {node=} {seen=} dsts={pairs[node]}")
+    for dst in pairs[node]:
+        if dst == "start":
+            continue
+        prev = seen[dst]
+        do_it = True
+        if dst.islower():
+            if prev == 2:
+                logging.debug(f"Skipping {dst} (2)")
+                do_it = False
+            elif prev == 1:
+                for n, c in seen.items():
+                    if c == 2 and n.islower() and n != dst:
+                        logging.debug(f"Skipping {dst} because {n}={c}")
+                        do_it = False
+                        break
+
+        if do_it:
+            yield from explore2(dst, pairs, seen, path + [dst])
+
+
+def compute2(pairs: dict[str, list[str]]) -> int:
+    paths = [path for path in explore2("start", pairs, defaultdict(int), ["start"])]
+    for path in paths:
+        logging.debug(f'{",".join(path)}')
+    return len(paths)
 
 
 def main():
     namespace = parse_args()
-    result1 = compute1(load_data(namespace))
-    print(f"{result1=}")
+    # result1 = compute1(load_data(namespace))
+    # print(f"{result1=}")
     result2 = compute2(load_data(namespace))
     print(f"{result2=}")
 
