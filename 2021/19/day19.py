13707e1c0613e01cac581ada730c8e877348d933
George V. Reilly <george@reilly.org>
2021-12-16 20:14:38 -0800

    Stubs

---
+++ 2021/19/day19.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+from __future__ import annotations
+
+DAY = 19
+# https://adventofcode.com/2021/day/19
+
+import argparse
+import logging
+import os
+
+
+TEST_DATA = f"day{DAY:02}test_input.txt"
+REAL_DATA = f"day{DAY:02}input.txt"
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description=f"AdventOfCode: Day {DAY}")
+    parser.set_defaults(
+        input_filename=None,
+        real=True,
+        verbose=False,
+        custom=False,
+    )
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        "--test", "-t", dest="real", action="store_false",
+        help=f"Use {TEST_DATA!r} as input_filename")
+    group.add_argument(
+        "--real", "-r", dest="real", action="store_true",
+        help="Use {REAL_DATA!r} as input_filename")
+
+    parser.add_argument(
+        "--steps", "-s", type=int,
+        help="Number of steps")
+    parser.add_argument(
+        "--custom", "-C", action="store_true",
+        help="Use custom data")
+    parser.add_argument(
+        "--verbose", "-v", action="store_true",
+        help="More verbose logging")
+    namespace = parser.parse_args()
+
+    namespace.input_filename = REAL_DATA if namespace.real else TEST_DATA
+    log_level = logging.DEBUG if namespace.verbose else logging.INFO
+    logging.basicConfig(level=log_level)
+
+    return namespace
+
+
+def read_data(input_filename: str):
+    with open(os.path.join(os.path.dirname(__file__), input_filename)) as f:
+        return f.readlines()
+
+
+def parse_data(text_data: list[str]) -> list[list[int]]:
+    return [[int(d) for d in line.strip()]
+            for line in text_data]
+
+
+def load_data(namespace) -> list[list[int]]:
+    if namespace.custom:
+        text_data = ["-1"]
+        data = parse_data(text_data)
+    else:
+        text_data = read_data(namespace.input_filename)
+        logging.info("%s", namespace.input_filename)
+        logging.debug("%s", text_data)
+        data = parse_data(text_data)
+    return data
+
+
+def grid_to_str(grid: list[list[int]], prefix="") -> str:
+    return prefix + ("\n" + prefix).join(
+        "".join([str(n) for n in row]) for row in grid) + "\n"
+
+
+
+def compute1(grid: list[list[int]]) -> int:
+    return 0
+
+
+def compute2(tile: list[list[int]]) -> int:
+    return 0
+
+
+def main():
+    namespace = parse_args()
+    result1 = compute1(load_data(namespace))
+    print(f"{result1=}")
+    result2 = compute2(load_data(namespace))
+    print(f"{result2=}")
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
